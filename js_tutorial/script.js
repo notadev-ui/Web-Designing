a31dce05d16380da84916f5b06a2a2a55a5912c2
notadev-ui <noteadeveloper@gmail.com>
2024-10-06 05:47:06 +0530

    css and js tutorial

---
+++ js_tutorial/script.js
@@ -0,0 +1,24 @@
+const profile = {
+     username : "not_a.dev",
+     isFollow : false,
+     Followers : 20,
+     Following : 24,
+     posts : 0,
+     Bio : "life is unfair"
+
+
+};
+
+console.log(profile);
+
+
+let age = "5";
+if (age>18){
+    console.log(" adult");
+}
+else if (age == 18){
+    console.log("valid");
+} else {
+    console.log("not adult");
+};
+
